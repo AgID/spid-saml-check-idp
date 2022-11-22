06e1e479dc4a6c5519fa68cc6d12e1ecc422bdde
damikael <michele.damico@agid.gov.it>
2022-11-22 18:28:06 +0100

    style: log page

--- src/client/src/views/SAMLLog/view.js
+++ src/client/src/views/SAMLLog/view.js
@@ -9,7 +9,7 @@ import "./style.css";
 function view(me) { 
     return (
         <div id="SAMLLog" className="animated fadeIn">
-            <p className="title h3">Web Browser SSO Log</p>
+            <p className="title h3">Web Browser SSO Last Execution Log</p>
             { me.state.report && me.state.report.lastlog && me.state.report.lastlog.details && (
             <div className="row">
                 <div className="col-md-8">
@@ -18,8 +18,20 @@ function view(me) {
                             <div className="col-md-12">
                                 <div className="row"> 
                                     <div className="col-sm-12">
-                                        <p className="h4">Last execution log</p>
-                                        <AceEditor mode="json" code={me.state.report.lastlog.details} />
+                                        <p className="h4"><a id="metadata" className="anchor">Metadata</a></p>
+                                        <AceEditor mode="xml" code={me.state.report.lastlog.details.metadata.configuration} />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <p className="h4 mt-5"><a id="authnrequest" className="anchor">AuthnRequest</a></p>
+                                        <AceEditor mode="xml" code={me.state.report.lastlog.details.authrequest.Message} />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <p className="h4 mt-5"><a id="authnresponse" className="anchor">AuthnResponse</a></p>
+                                        <AceEditor mode="xml" code={me.state.report.lastlog.details.authresponse.Message} />
+                                    </div>
+                                    <div className="col-sm-12">
+                                        <p className="h4 mt-5"><a id="log" className="anchor">Report Log</a></p>
+                                        <AceEditor mode="json" code={me.state.report.lastlog.details.report} />
                                     </div>
                                 </div>
                             </div>
@@ -31,17 +43,10 @@ function view(me) {
                     <Sticky stickyClassName="sticky-tools" topOffset={-50}>
                         <div className="tools">
                             <div className="col-sm-12">
-                                {/*
-                                <label className="switch switch-success me-3">
-                                    <input type="checkbox" className="switch-input" 
-                                        checked={me.state.detailview}
-                                        onChange={(e)=>{me.setDetailView(e.target.checked)}}>
-                                    </input>
-                                    <span className="switch-slider"></span>
-                                </label>
-                                <span className="align-super">Visualizzazione dettaglio</span>
-                                <hr/>
-                                */}
+                                <a href="#metadata" className="btn btn-primary">Metadata</a>
+                                <a href="#authnrequest" className="btn btn-primary">AuthnRequest</a>
+                                <a href="#authnresponse" className="btn btn-primary">AuthnResponse</a>
+                                <a href="#log" className="btn btn-primary">Report Log</a>
                                 <button type="button" className="btn btn-success"
                                     onClick={()=>{me.print()}}>
                                     <i className="fa fa-print" aria-hidden="true"></i> Print
